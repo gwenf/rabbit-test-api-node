d2144835f156fba8ec9891d6f962765383bbf61b
Gwen Faraday <gwenfaraday@gmail.com>
2018-08-24 08:26:27 -0400

    testing heroku

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-// app.use('/api/', routes);
+app.use('/api/', routes);
 
 // models.sequelize.sync().then(() => {
   app.listen(port, '0.0.0.0', () => {
