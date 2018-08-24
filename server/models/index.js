172734e3c7f760d2c107e5c7172dc332463e3a13
Gwen Faraday <gwenfaraday@gmail.com>
2018-08-24 09:49:39 -0400

    updating sql connections

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,8 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    logging: true
+    logging: true,
+    minConnections: 0
   });
 } else {
   sequelize = new Sequelize('student_management', 'gzepeda', 'postgres', {
