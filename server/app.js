427ac828114f90ddb3c9a7685f71d84f042738f0
Gwen Faraday <gwenfaraday@gmail.com>
2018-08-23 22:54:51 -0400

    adding heroku setup

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ const bodyParser = require('body-parser');
 const port = process.env.NODE_ENV || '4000';
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
